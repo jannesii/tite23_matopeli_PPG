c4e886318970083413356a9e214c57d8846c2bd3
Katajisto <Kai.Katajisto@seamk.fi>
2024-10-01 11:25:49 +0300

    perkele

--- matopeli.py
+++ matopeli.py
@@ -1,4 +1,6 @@
-# 'pip install PySide6' tarvitaan 
+# 'pip install PySide6' tarvitaan
+# Tämä on testi, terveisin Kai Katajisto
+
 import sys
 import random
 from PySide6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QMenu
